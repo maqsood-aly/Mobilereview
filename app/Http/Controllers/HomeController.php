68f0af47c54186b2a5728ae51cca0a55fc5596f9
Maqsood ali <maqsoodmn03@gmail.com>
2024-07-27 13:29:24 +0500

    searched fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\DB; // Import DB facade
 use App\Models\Feature;
 use Illuminate\Support\Facades\Log;
@@ -19,12 +20,12 @@ class HomeController extends Controller
         // Perform your queries directly
         $products = Product::published()->take(12)->get(['name', 'price', 'image_url', 'slug']);
         $latestProducts = Product::published()->latest()->take(10)->get(['name', 'price', 'image_url', 'slug']);
-        
+
         // Render the view and pass the data
         return view('user.home', compact('products', 'latestProducts'));
     }
-    
- 
+
+
 
 
     public function show($slug)
@@ -39,7 +40,7 @@ class HomeController extends Controller
             $query->where('base_model', $products->model->base_model);
         })->where('id', '!=', $products->id)->take(12)->get();
 
-       
+
         // Fetch features for the product
         $features = Product_feature::with('feature')
             ->where('product_id', $products->id)
@@ -47,52 +48,54 @@ class HomeController extends Controller
 
         // Fetch latest products
         $latestProducts = Product::latest()->take(12)->get();
-        $product_url = true ; 
-        return view('user.singlepost', compact('products', 'features', 'relatedProducts', 'latestProducts','product_url'));
+        $product_url = true;
+        return view('user.singlepost', compact('products', 'features', 'relatedProducts', 'latestProducts', 'product_url'));
     }
 
     function price($price)
     {
         $products = Product::where('price', '<=', $price)
             ->where('price', '>', $price - 10000)
-            ->take(12)->get(['name', 'price', 'image_url','slug']);
+            ->take(12)->get(['name', 'price', 'image_url', 'slug']);
         $latestProducts = Product::latest()->take(12)->get();
         $card_title = 'Mobiles Under Price ' . number_format($price) . ' Pkr';
         $price_filter = true;
-        return view('user.home', compact('products', 'latestProducts', 'card_title','price_filter'));
+        return view('user.home', compact('products', 'latestProducts', 'card_title', 'price_filter'));
     }
 
     function brand($brand)
     {
         $brand = Brand::where('name', $brand)->firstOrFail();
-        $products = Product::where('brand_id', $brand->id)->take(12)->get(['name', 'price', 'image_url','slug']);
+        $products = Product::where('brand_id', $brand->id)->take(12)->get(['name', 'price', 'image_url', 'slug']);
         $latestProducts = Product::latest()->take(12)->get();
         $card_title = 'Mobiles of ' . '"' . $brand->name . '"' . ' brand';
         $brand_filter = true;
-        return view('user.home', compact('products', 'latestProducts', 'card_title','brand_filter'));
+        return view('user.home', compact('products', 'latestProducts', 'card_title', 'brand_filter'));
     }
 
 
- 
+
     public function compare($device1, $device2)
     {
         $product1 = Product::where('slug', $device1)
-        ->with(['brand', 'model', 'features', 'pros', 'cons'])
-        ->firstOrFail();
+            ->with(['brand', 'model', 'features', 'pros', 'cons'])
+            ->firstOrFail();
 
         $product2 = Product::where('slug', $device2)
-        ->with(['brand', 'model', 'features', 'pros', 'cons'])
-        ->firstOrFail();
+            ->with(['brand', 'model', 'features', 'pros', 'cons'])
+            ->firstOrFail();
 
 
         // $product2 = Product::where('slug', $device2)->firstOrFail();
 
-        return view('user.comparison', compact('product1','product2'));
+        return view('user.comparison', compact('product1', 'product2'));
     }
     public function search_products(Request $request)
     {
-        $products = Product::where('name', 'like', '%' . $request->query('query') . '%')->get(['name', 'slug']);
-    
+        $products = Product::where('status', 'published')
+        ->where('name', 'like', '%' . $request->query('query') . '%')
+        ->get(['name', 'slug']);
+
         return response()->json([
             'products' => $products
         ]);
@@ -106,6 +109,4 @@ class HomeController extends Controller
     {
         return view('user.about');
     }
-    
-
 }
